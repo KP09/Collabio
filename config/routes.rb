d9bb0f912a60ba59718d66a9e36c5623ef0cb07d
Kees Postma <kk.postma@gmail.com>
2017-06-05 15:25:44 +0100

    Adds primary routes and search view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Homepage
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Search results
+  get '/search', to: 'pages#search'
+
+  # Users
+  devise_for :users
+  resources :users, except: [:index]
+
+  # Projects
+  resources :projects, except: [:index]
+
+  # Participations
+  resources :participations, only: [:create, :destroy]
+
+  # Contributions
+  resources :contributions, only: [:create, :update, :destroy]
+
+  # Upvotes
+  resources :upvotes, only: [:create, :destroy]
 end
