c9c41126339f6140bb4b4b4f06ebe754fc18f072
Rami Bakri <rbakri1@gmail.com>
2017-06-08 17:44:49 +0100

    completed modal view for creating a new project

--- app/controllers/company/dashboard_controller.rb
+++ app/controllers/company/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Company::DashboardController < Company::CompanyController
   def index
   	@user = current_user
+  	@project = Project.new
   	@active_projects = @user.active_projects
   	@closed_projects = @user.closed_projects
   end
