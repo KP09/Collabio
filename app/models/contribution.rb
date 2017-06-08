13882e47b54383b8063287675ad20ddfafae658c
Rami Bakri <rbakri1@gmail.com>
2017-06-08 12:59:37 +0100

    saving work done

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,10 +4,11 @@ class Contribution < ApplicationRecord
   belongs_to :project
   has_many :upvotes, dependent: :destroy
 
+  # Attachinary method
   has_attachment :file
 
   # Validations
   validates :comment, presence: true
-# validates :file, presence: true
+	# Validates :file, presence: true
   validates :project_id, uniqueness: { scope: :user_id }
 end
