49276b1701953d76bb65d5c19359e17efa6e1d65
Kees Postma <kk.postma@gmail.com>
2017-06-08 21:21:48 +0100

    animated alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,4 +28,9 @@ $(document).ready(function(){
       });
 
     });
+    window.setTimeout(function() {
+      $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove();
+      });
+    }, 1200);
 });
