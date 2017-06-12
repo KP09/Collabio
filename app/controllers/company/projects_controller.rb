53acb9a2c78eb592bc082d6a531003f6ed7fdb08
James Block <james@hewines.com>
2017-06-12 17:06:41 +0100

    fixes to company dash

--- app/controllers/company/projects_controller.rb
+++ app/controllers/company/projects_controller.rb
@@ -1,5 +1,11 @@
 class Company::ProjectsController < Company::CompanyController
+  before_action :set_project
   def show
+  end
+
+  private
+
+  def set_project
     @project = Project.find(params[:id])
   end
 end
