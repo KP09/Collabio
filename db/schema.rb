6735925b582d2fd89ebb3b71dab24343a9bf9842
Kees Postma <kk.postma@gmail.com>
2017-06-13 11:25:57 +0100

    Adds education and occupation to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608115651) do
+ActiveRecord::Schema.define(version: 20170613074239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,8 @@ ActiveRecord::Schema.define(version: 20170608115651) do
     t.string "location"
     t.boolean "company", default: false
     t.boolean "admin", default: false
+    t.string "education"
+    t.string "profession"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
